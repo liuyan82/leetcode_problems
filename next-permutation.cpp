605065812078892383e558c3f82454dc54d93fff
maxyanliu <maxyanliu@tencent.com>
2024-03-09 15:21:03 +0800

    ..

---
+++ next-permutation.cpp
@@ -0,0 +1,77 @@
+/********
+ 整数数组的一个 排列  就是将其所有成员以序列或线性顺序排列。
+
+例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。
+整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，那么数组的 下一个排列 就是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。
+
+例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。
+类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。
+而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。
+给你一个整数数组 nums ，找出 nums 的下一个排列。
+
+必须 原地 修改，只允许使用额外常数空间。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3]
+输出：[1,3,2]
+示例 2：
+
+输入：nums = [3,2,1]
+输出：[1,2,3]
+示例 3：
+
+输入：nums = [1,1,5]
+输出：[1,5,1]
+ 
+
+提示：
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+***********/
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.empty()) {
+            return;
+        }
+        int sort_start = -1;
+        int local_max = nums[nums.size() - 1];
+        int local_max_index = nums.size() - 1;
+        
+        for (int i = nums.size() - 2; i >= 0; --i) {
+            if (local_max > nums[i]) {
+                int local_min_index = local_max_index;
+                int local_min = local_max;
+
+                for (int j = i + 1; j < nums.size(); ++j) {
+                    if (nums[j] < local_min && nums[j] > nums[i]) {
+                        local_min = nums[j];
+                        local_min_index = j;
+                    }
+                }
+
+                nums[local_min_index] = nums[i];
+                nums[i] = local_min;
+                sort_start = i + 1;
+                
+                break;
+            } else {
+                if (nums[i] > local_max) {
+                    local_max = nums[i];
+                    local_max_index = i;
+                }
+            }
+        }
+
+        if (sort_start >= 0) {
+            std::sort(nums.begin() + sort_start, nums.end());
+        } else {
+            std::reverse(nums.begin(), nums.end());
+        }
+    }
+};
